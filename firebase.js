46ba6fa0fe08dfc692cbe007a4f14e1e784c474b
ScotDev <callum.git@outlook.com>
2023-07-04 16:17:51 +0100

    Updated cors

--- firebase.js
+++ firebase.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 // const stringifiedKey = JSON.stringify(
 //   process.env.FIREBASE_ADMIN_PRIVATE_KEY
 // ).replace(/\\n/g, "\n");
-// console.log(stringifiedKey);
+console.log(process.env.FIREBASE_ADMIN_PRIVATE_KEY);
 
 const app = admin.initializeApp({
   // credential: admin.credential.cert(serviceAccount),
