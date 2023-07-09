9a7732d274a7a486361518d94a646156e77f4df2
ScotDev <callum.git@outlook.com>
2023-07-09 15:26:07 +0100

    Added new /weather/all route with better formatted response

--- routes/weather.js
+++ routes/weather.js
@@ -5,33 +5,33 @@ const axios = require("axios");
 require("dotenv").config();
 
 // Should check auth state
-router.post("/now", async (req, res) => {
-  const { lat, long } = req.body;
+// router.post("/now", async (req, res) => {
+//   const { lat, long } = req.body;
 
-  //   Should check if correct params/body contents sent
-  //   console.log(lat, long);
-  console.log(req.body);
+//   //   Should check if correct params/body contents sent
+//   //   console.log(lat, long);
+//   console.log(req.body);
 
-  //   Get weatherData for current location with given coords
-  if (lat && long) {
-    try {
-      const apiRes = await axios.get(
-        `https://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=${lat},${long}&aqi=no`
-      );
-      //   console.log(apiRes.data);
-      return res.status(200).send(apiRes.data);
-    } catch (error) {
-      console.log(error);
-      res.status(404).send({ erroMsg: "Error contacting weather API" });
-    }
-  }
+//   //   Get weatherData for current location with given coords
+//   if (lat && long) {
+//     try {
+//       const apiRes = await axios.get(
+//         `https://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=${lat},${long}&aqi=no`
+//       );
+//       //   console.log(apiRes.data);
+//       return res.status(200).send(apiRes.data);
+//     } catch (error) {
+//       console.log(error);
+//       res.status(404).send({ error: "Error contacting weather API" });
+//     }
+//   }
 
-  //   Parse out and return what's needed
-  //   .then(response => res.json(response.data))
+//   //   Parse out and return what's needed
+//   //   .then(response => res.json(response.data))
 
-  //   Handle errors
-  return res.status(400).send({ erroMsg: "No coords recieved" });
-});
+//   //   Handle errors
+//   return res.status(400).send({ error: "No coords recieved" });
+// });
 
 router.get("/daily", async (req, res) => {
   const { lat, long } = req.query;
@@ -39,15 +39,7 @@ router.get("/daily", async (req, res) => {
   const formattedDate = date.toLocaleString();
   console.log(formattedDate, req.query);
   const updatedTime = Date.now();
-  // const options = {
-  //   year: "numeric",
-  //   month: "2-digit",
-  //   day: "2-digit",
-  // };
 
-  // const formattedDate = new Intl.DateTimeFormat("en-GB", options)
-  //   .format(updatedTime)
-  //   .replace(/\//g, "-");
   if (lat && long) {
     try {
       const apiRes = await axios.get(
@@ -61,13 +53,6 @@ router.get("/daily", async (req, res) => {
       const dailyData = [];
 
       await forecastDataArr.forEach((item) => {
-        // If item.date_epoch !== today, push to dailyData
-        // Generate code to format Date.now() to match item.date_epoch format
-        // const APIdate = new Date(item.date);
-        // const formattedDate2 = new Intl.DateTimeFormat("en-GB", options)
-        //   .format(APIdate)
-        //   .replace(/\//g, "-");
-
         dailyData.push({
           date: item.date,
           condition: item.day.condition.text,
@@ -88,12 +73,12 @@ router.get("/daily", async (req, res) => {
       });
     } catch (error) {
       console.log(error);
-      return res.status(404).send({ erroMsg: "Error contacting weather API" });
+      return res.status(404).send({ error: "Error contacting weather API" });
     }
   }
 
   //   Handle errors
-  return res.status(400).send({ erroMsg: "No coords recieved" });
+  return res.status(400).send({ error: "No coords recieved" });
 });
 // router.post("/daily", async (req, res) => {
 //   const { lat, long } = req.body;
@@ -134,12 +119,12 @@ router.get("/daily", async (req, res) => {
 //       });
 //     } catch (error) {
 //       console.log(error);
-//       res.status(404).send({ erroMsg: "Error contacting weather API" });
+//       res.status(404).send({ error: "Error contacting weather API" });
 //     }
 //   }
 
 //   //   Handle errors
-//   return res.status(400).send({ erroMsg: "No coords recieved" });
+//   return res.status(400).send({ error: "No coords recieved" });
 // });
 
 // router.post("/hourly", async (req, res) => {
@@ -191,12 +176,12 @@ router.get("/daily", async (req, res) => {
 //       });
 //     } catch (error) {
 //       console.log(error);
-//       return res.status(404).send({ erroMsg: "Error contacting weather API" });
+//       return res.status(404).send({ error: "Error contacting weather API" });
 //     }
 //   }
 
 //   //   Handle errors
-//   return res.status(400).send({ erroMsg: "No coords recieved" });
+//   return res.status(400).send({ error: "No coords recieved" });
 // });
 router.get("/hourly", async (req, res) => {
   const { lat, long } = req.query;
@@ -247,12 +232,109 @@ router.get("/hourly", async (req, res) => {
       });
     } catch (error) {
       console.log(error);
-      return res.status(404).send({ erroMsg: "Error contacting weather API" });
+      return res.status(404).send({ error: "Error contacting weather API" });
     }
   }
 
   //   Handle errors
-  return res.status(400).send({ erroMsg: "No coords recieved" });
+  return res.status(400).send({ error: "No coords recieved" });
+});
+
+const formatCountry = (country) => {
+  if (country === "United Kingdom") {
+    return "UK";
+  } else if (country === "United States of America") {
+    return "USA";
+  } else {
+    return country;
+  }
+};
+router.get("/all", async (req, res) => {
+  const { lat, long } = req.query;
+
+  console.log(req.query);
+
+  if (lat && long) {
+    try {
+      const apiRes = await axios.get(
+        `https://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_API_KEY}&q=${lat},${long}&days=7&aqi=no&alerts=no`
+      );
+
+      // Split data into now, daily, and hourly
+      // Now
+      const { temp_c, condition, wind_mph, humidity, vis_miles } =
+        apiRes.data.current;
+      const conditionText = condition.text;
+      const { name, country } = apiRes.data.location;
+      const formattedCountry = formatCountry(country);
+      const { maxtemp_c, mintemp_c } = apiRes.data.forecast.forecastday[0].day;
+      // Hourly
+      const hourlyData = [];
+      const todayForecastDataArr = await apiRes.data.forecast.forecastday[0]
+        .hour;
+      const tomorrowForecastDataArr = await apiRes.data.forecast.forecastday[1]
+        .hour;
+      const now = Date.now() / 1000;
+      await todayForecastDataArr.forEach((item) => {
+        if (item.time_epoch > now) {
+          hourlyData.push({
+            time: item.time,
+            condition: item.condition.text,
+            temp_c: Math.trunc(item.temp_c),
+          });
+        }
+      });
+
+      const limit = 24 - hourlyData.length;
+      for (let index = 0; index < limit; index++) {
+        const item = tomorrowForecastDataArr[index];
+        hourlyData.push({
+          time: item.time,
+          condition: item.condition.text,
+          temp_c: Math.trunc(item.temp_c),
+        });
+      }
+
+      // Daily
+      const dailyForecastDataArr = await apiRes.data.forecast.forecastday;
+      const dailyData = [];
+
+      await dailyForecastDataArr.forEach((item) => {
+        dailyData.push({
+          date: item.date,
+          condition: item.day.condition.text,
+          maxtemp_c: Math.trunc(item.day.maxtemp_c),
+          mintemp_c: Math.trunc(item.day.mintemp_c),
+          maxwind_mph: item.day.maxwind_mph,
+          avgvis_miles: item.day.avgvis_miles,
+          avghumidity: item.day.avghumidity,
+          uv: item.day.uv,
+          //   Annoyingly wind direction only available at hourly level
+        });
+      });
+
+      return res.status(200).send({
+        now: {
+          temp_c: Math.trunc(temp_c),
+          condition: conditionText,
+          wind_mph: Math.trunc(wind_mph),
+          humidity,
+          vis_miles,
+          maxtemp_c: Math.trunc(maxtemp_c),
+          mintemp_c: Math.trunc(mintemp_c),
+        },
+        location: { name, country: formattedCountry },
+        data: { hourlyData, dailyData },
+      });
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(404)
+        .send({ error: "Error contacting external resource" });
+    }
+  }
+
+  return res.status(400).send({ error: "No coords recieved" });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cors(corsOptions));
 
 const limiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 15 minutes
-  max: 20, // limit each IP to 100 requests per windowMs
+  max: process.env.IS_DEV ? 300 : 30, // limit each IP to 100 requests per windowMs
   message: "Too many requests from this IP, please try again later",
 });
 app.use(limiter);
